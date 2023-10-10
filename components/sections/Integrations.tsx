d3a01c21427d6ca67d838c20420b98fe0899df7b
ensi <66754841+ncor@users.noreply.github.com>
2023-10-11 01:14:44 +0300

    fix: integrations sections displayment

--- components/sections/Integrations.tsx
+++ components/sections/Integrations.tsx
@@ -3,7 +3,7 @@ import IntegrationsImage from '@/public/integrations.svg';
 
 
 export default function Integrations() {
-    return <div className="w-full px-6 flex justify-center bg-black overflow-x-hidden">
+    return <div className="-z-10 w-full px-6 flex justify-center backdrop-blur-[160px] overflow-x-hidden">
         <div className="reverse w-full py-[84px] flex justify-center">
             <Image src={ IntegrationsImage } alt="Интеграции"/>
         </div>
